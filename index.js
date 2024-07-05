1d8af90cb3427665e4075e5d1ab9827aa756deb7
whatsssmeee <69847505+whatsssmeee@users.noreply.github.com>
2024-07-05 23:37:50 +0300

    index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(🔗 Listening to RTX: http://localhost:${port});
-  console.log(🔗 Powered By RTX);
+  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
+  console.log(`🔗 Powered By RTX`);
 });
 
 const statusMessage = "Bot made by Requited";
@@ -25,7 +25,7 @@ const channelId = '';  // Set your channel ID if you want to send messages to a 
 async function login() {
   try {
     await client.login(process.env.TOKEN);
-    console.log(\x1b[36m%s\x1b[0m, |    🐇 Logged in as ${client.user.tag});
+    console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
   } catch (error) {
     console.error('Failed to log in:', error);
     process.exit(1);
@@ -33,9 +33,9 @@ async function login() {
 }
 
 client.once('ready', () => {
-  console.log(\x1b[36m%s\x1b[0m, |    ✅ Bot is ready as ${client.user.tag});
-  console.log(\x1b[36m%s\x1b[0m, |    ✨HAPPY NEW YEAR MY DEAR FAMILY);
-  console.log(\x1b[36m%s\x1b[0m, |    ❤️WELCOME TO 2024);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
 
   client.user.setPresence({
     activities: [{ name: statusMessage, type: ActivityType.Playing }],
@@ -44,14 +44,14 @@ client.once('ready', () => {
 
   const textChannel = client.channels.cache.get(channelId);
   if (textChannel instanceof TextChannel) {
-    textChannel.send(Bot status is: ${statusMessage});
+    textChannel.send(`Bot status is: ${statusMessage}`);
   }
 
-  logToRenderHost(Bot is ready and status set to: ${statusMessage});
+  logToRenderHost(`Bot is ready and status set to: ${statusMessage}`);
 });
 
 function logToRenderHost(message) {
-  const logMessage = [${new Date().toISOString()}] ${message}\n;
+  const logMessage = `[${new Date().toISOString()}] ${message}\n`;
   fs.appendFile(path.join(__dirname, 'logs.txt'), logMessage, (err) => {
     if (err) console.error('Failed to log message to render host:', err);
   });
