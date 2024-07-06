a0ce4ddd62aabd82dc5c3106312adf60c42f8b98
whatsssmeee <69847505+whatsssmeee@users.noreply.github.com>
2024-07-06 21:01:12 +0300

    index.js

--- index.js
+++ index.js
@@ -1,40 +1,80 @@
+/**
+ ██████╗░████████╗██╗░░██╗           
+ ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+ ██████╔╝░░░██║░░░░╚███╔╝░          
+ ██╔══██╗░░░██║░░░░██╔██╗░          
+ ██║░░██║░░░██║░░░██╔╝╚██╗          
+ ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+  GIT : https://github.com/RTX-GAMINGG/Bot-ghost-status-remover-by-RTX
+  DISCORD SERVER : https://discord.gg/FUEHs7RCqz
+  YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+ * **********************************************
+ *   Code by RTX GAMING
+ * **********************************************
+ */
+
 const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 require('dotenv').config();
-
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildPresences // Ensure this is included
+    GatewayIntentBits.GuildPresences
   ],
+});
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('YaY Your Bot Status Changed✨');
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
+  console.log(`🔗 Powered By RTX`);
 });
 
 const statusMessages = ["Made by Requited", "Free the guys!"];
 let currentIndex = 0;
 
+function updateStatusAndSendMessages() {
+  const currentStatus = statusMessages[currentIndex];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Playing }],
+    status: 'online'
+  }).then(() => console.log(`Presence set to: Playing ${currentStatus}`))
+    .catch(console.error);
+
+  currentIndex = (currentIndex + 1) % statusMessages.length;
+}
+
 client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-  
-  // Function to update the bot's status
-  function updateStatus() {
-    const currentStatus = statusMessages[currentIndex];
-    client.user.setPresence({
-      activities: [{ name: currentStatus, type: ActivityType.Playing }],
-      status: 'online'
-    }).then(() => console.log(`Presence set to: Playing ${currentStatus}`))
-      .catch(console.error);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  updateStatusAndSendMessages();
 
-    // Move to the next status
-    currentIndex = (currentIndex + 1) % statusMessages.length;
-  }
-
-  // Initial status update
-  updateStatus();
-
-  // Update status every 10 seconds
-  setInterval(updateStatus, 10000);
+  setInterval(() => {
+    updateStatusAndSendMessages();
+  }, 10000); // Update status every 10 seconds
 });
 
-// Log the bot in
-client.login(process.env.TOKEN).catch(console.error);
+login();
+
+/**
+ ██████╗░████████╗██╗░░██╗           
+ ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+ ██████╔╝░░░██║░░░░╚███╔╝░          
+ ██╔══██╗░░░██║░░░░██╔██╗░          
+ ██║░░██║░░░██║░░░██╔╝╚██╗          
+ ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+GIT : https://github.com/RTX-GAMINGG/Bot-ghost-status-remover-by-RTX
+  DISCORD SERVER : https://discord.gg/FUEHs7RCqz
+  YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+ * **********************************************
+ *   Code by RTX GAMING
+ * **********************************************
+ */
