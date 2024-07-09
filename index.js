3b47e1a8dd088a95241008a222443d6042eba414
whatsssmeee <69847505+whatsssmeee@users.noreply.github.com>
2024-07-09 09:45:36 +0300

    index.js

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(🔗 Listening to RTX: http://localhost:${port});
-  console.log(🔗 Powered By RTX);
+  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
+  console.log(`🔗 Powered By RTX`);
 });
 
 const statusMessages = ["Playing /help", "Free the guys!"];
@@ -40,7 +40,7 @@ const channelId = '';  // Set your channel ID if you want to send messages to a 
 async function login() {
   try {
     await client.login(process.env.TOKEN);
-    console.log(\x1b[36m%s\x1b[0m, |    🐇 Logged in as ${client.user.tag});
+    console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
   } catch (error) {
     console.error('Failed to log in:', error);
     process.exit(1);
@@ -74,18 +74,18 @@ function updateStatusAndSendMessages() {
   const textChannel = client.channels.cache.get(channelId);
 
   if (textChannel instanceof TextChannel) {
-    textChannel.send(Bot status is: ${currentStatus});
+    textChannel.send(`Bot status is: ${currentStatus}`);
   } else {
-    console.log(Status set to: ${currentStatus});
+    console.log(`Status set to: ${currentStatus}`);
   }
 
   currentIndex = (currentIndex + 1) % statusMessages.length;
 }
 
 client.once('ready', () => {
-  console.log(\x1b[36m%s\x1b[0m, |    ✅ Bot is ready as ${client.user.tag});
-  console.log(\x1b[36m%s\x1b[0m, |    ✨HAPPY NEW YEAR MY DEAR FAMILY);
-  console.log(\x1b[36m%s\x1b[0m, |    ❤️WELCOME TO 2024);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
